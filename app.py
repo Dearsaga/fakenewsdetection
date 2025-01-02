5072f2af952a8ff67b6049e605f64056365002bc
sk18skills <144412254+sk18skills@users.noreply.github.com>
2025-01-02 11:40:42 +0530

    Add initial project files for Fake News Detection
    - Added dataset sourced from Kaggle containing labeled fake and real news articles - Implemented NLP preprocessing pipeline (tokenization, stemming, lemmatization) - Included TF-IDF vectorization to transform text data into numerical features - Added Decision Tree Classifier with an accuracy of 99.52% for predicting news authenticity - Built web application using Flask for real-time analysis of news articles - Updated README with project details, features, and technologies used

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request,jsonify
+import joblib
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import nltk
+
+app = Flask(__name__)
+#app = Flask(__name__, static_folder='K:\\fake\\static', static_url_path='/static')
+#app = Flask(__name__, static_folder='static', static_url_path='/static')
+
+# Define stemming function 
+port_stem = PorterStemmer()
+def stemming(content):
+    con = re.sub('[^a-zA-Z]', ' ', content)
+    con = con.lower()
+    con = con.split()
+    con = [port_stem.stem(word) for word in con if not word in stopwords.words('english')]
+    con = ' '.join(con)
+    return con
+# Download stopwords
+nltk.download('stopwords')
+
+# Load the model and vectorizer
+model = joblib.load('model.pkl')
+vectorizer = joblib.load('vector.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form.get('news')
+    print("Received text:", text) # Use .get to avoid KeyError 
+    if not text: 
+        return jsonify({"error": "No input text provided"}), 400
+    
+    # Preprocess the text
+    preprocessed_text = stemming(text)
+    
+    vectorized_text = vectorizer.transform([text])
+    prediction = model.predict(vectorized_text)
+    
+    result = "Fake News" if prediction[0] == 1 else "Real News"
+    return jsonify(result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
